18a2ffccb97dbdc39f405f084c4f748662d1be05
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-05-16 14:00:41 +0600

    database sql file added

---
+++ BlogX.sql
@@ -0,0 +1,123 @@
+USE BlogX;
+
+CREATE TABLE ApplicationUser(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	UserName VARCHAR(50) NOT NULL,
+	NormalizeUserName VARCHAR(50) NOT NULL,
+	Email VARCHAR(50) NOT NULL,
+	NormalizeEmail VARCHAR(50) NOT NULL,
+	FullName VARCHAR(50) NULL,
+	PasswordHash VARCHAR(MAX) NOT NULL
+);
+
+CREATE INDEX [IX_ApplicationUser_NormalizeUserName] ON [dbo].[ApplicationUser]([NormalizeUserName]);
+
+CREATE INDEX [IX_ApplicationUser_NormalizeEmail] ON [dbo].[ApplicationUser] ([NormalizeEmail]);
+
+CREATE TABLE Photo(
+	Id INT IDENTITY(1, 1),
+	ApplicationUserID INT NOT NULL,
+	PublicId VARCHAR(50) NOT NULL,
+	ImageUrl VARCHAR(MAX) NOT NULL,
+	[Description] VARCHAR(50) NOT NULL,
+	PublishDate DATETIME NOT NULL DEFAULT GETDATE(),
+	UpdateDate DATETIME NOT NULL DEFAULT GETDATE(),
+	PRIMARY KEY(ID),
+	FOREIGN KEY(ApplicationUserID) REFERENCES ApplicationUser(Id)
+);
+
+CREATE TABLE Blog(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	ApplicationUserID INT NOT NULL,
+	PhotoId INT NULL,
+	Title VARCHAR(50) NOT NULL,
+	Content VARCHAR(MAX) NOT NULL,
+	PublishDate DATETIME NOT NULL DEFAULT GETDATE(),
+	UpdateDate DATETIME NOT NULL DEFAULT GETDATE(),
+	IsActive BIT NOT NULL DEFAULT CONVERT(BIT, 1),
+	FOREIGN KEY(ApplicationUserID) REFERENCES ApplicationUser(Id),
+	FOREIGN KEY(PhotoId) REFERENCES Photo(Id)
+);
+
+CREATE TABLE BlogComment(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	ParentCommentId INT NULL,
+	BlogId INT NOT NULL,
+	ApplicationUserID INT NOT NULL,
+	Comment VARCHAR(500) NOT NULL,
+	PublishDate DATETIME NOT NULL DEFAULT GETDATE(),
+	UpdateDate DATETIME NOT NULL DEFAULT GETDATE(),
+	IsActive BIT NOT NULL DEFAULT CONVERT(BIT, 1),
+	FOREIGN KEY(BlogId) REFERENCES Blog(Id),
+	FOREIGN KEY(ApplicationUserID) REFERENCES ApplicationUser(Id)
+);
+
+CREATE SCHEMA [aggregate];
+
+CREATE VIEW [aggregate].[Blog]
+AS
+
+	SELECT 
+		Blog.Id,
+		Blog.ApplicationUserID,
+		AppUser.UserName,
+		Blog.Title,
+		Blog.Content,
+		Blog.PhotoId,
+		Blog.PublishDate,
+		Blog.UpdateDate,
+		Blog.IsActive
+	FROM 
+	dbo.Blog AS Blog 
+	INNER JOIN 
+	dbo.ApplicationUser AS AppUser 
+	ON Blog.ApplicationUserID = AppUser.Id;
+
+
+CREATE VIEW [aggregate].[BlogComment]
+AS
+
+	SELECT 
+		BlogComment.Id,
+		BlogComment.ParentCommentId,
+		BlogComment.BlogId,
+		BlogComment.Comment,
+		AppUser.UserName,
+		BlogComment.ApplicationUserID,
+		BlogComment.PublishDate,
+		BlogComment.UpdateDate,
+		BlogComment.IsActive
+	FROM 
+	dbo.BlogComment AS BlogComment 
+	INNER JOIN 
+	dbo.ApplicationUser AS AppUser 
+	ON BlogComment.ApplicationUserID = AppUser.Id;
+
+CREATE TYPE [dbo].[AccountType] AS TABLE(
+	[UserName] VARCHAR(20) NOT NULL,
+	[NormalizeUserName] VARCHAR(20) NOT NULL,
+	[Email] VARCHAR(30) NOT NULL,
+	[NormalizeEmail] VARCHAR(30) NOT NULL,
+	[FullName] VARCHAR(30) NULL,
+	[PasswordHash] NVARCHAR(MAX) NOT NULL
+);
+
+CREATE TYPE [dbo].[PhotoType] AS TABLE(
+	[PublicId] VARCHAR(50) NOT NULL,
+	[ImageUrl] VARCHAR(250) NOT NULL,
+	[Description] VARCHAR(30) NOT NULL
+);
+
+CREATE TYPE [dbo].[BlogType] AS TABLE(
+	[BlogId] INT NOT NULL,
+	[Title] VARCHAR(50) NOT NULL,
+	[Content] VARCHAR(MAX) NOT NULL,
+	[PhotoId] INT NULL
+);
+
+CREATE TYPE [dbo].[BlogCommentType] AS TABLE(
+	[Id] INT NOT NULL,
+	[ParentCommentId] INT NULL,
+	[BlogId] INT NOT NULL,
+	[Comment] VARCHAR(300) NOT NULL
+);
